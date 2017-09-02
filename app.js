8953c94a56702828c4b997429aa7e9edfa3c09e6
Lim Ji Ho <DJiho@Lims-MacBook-Pro-61.local>
2017-09-02 20:52:56 +0800

    sdasd

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/scripts"));
 app.use(express.static(__dirname + "/semantic"));
 
 app.get("/", function(req, res){
