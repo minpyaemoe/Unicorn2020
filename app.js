decd97b297187cb12f159e53c7ddfd21084fc8ab
Lim Ji Ho <DJiho@Lims-MacBook-Pro-61.local>
2017-09-02 20:41:12 +0800

    sdasd

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/", function(req, res){
 //     console.log('Unicon2018 is running:' + port);
 // });
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
 
-// app.listen(3000);
+app.listen(3000);
