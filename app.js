9d4557664e91456fe0b124e3210d2785f8c05dfa
ariadn3 <threadofethariadne@gmail.com>
2018-10-13 12:37:38 +0800

    Changed app.listen command

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get("/", function(req, res){
 	res.render("main");
 })
 
-//app.listen(process.env.PORT, process.env.IP);
-app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
+// app.listen(3000);
